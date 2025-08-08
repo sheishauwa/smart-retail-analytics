57a6ebe65a2dc87f42c455f00ac3a800380b3211
Hauwa Kulu Njidda <hauwa.dbtech@gmail.com>
2025-08-08 10:14:47 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "raw_zone" {
+  bucket = "smart-retail-raw-zone"
+}
+
+resource "aws_instance" "ingestion_ec2" {
+  ami           = "ami-08c40ec9ead489470" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  key_name      = var.key_pair_name
+
+  tags = {
+    Name = "RetailIngestionEC2"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.ingestion_ec2.public_ip
+}
 
